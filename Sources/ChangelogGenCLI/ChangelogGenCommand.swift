06a2430c9d873d35b6d7b2bdf0e1e038f8eec57f
tosaka07 <tosakaup@gmail.com>
2021-10-22 15:46:01 +0900

    Update to 0.0.5

--- Sources/ChangelogGenCLI/ChangelogGenCommand.swift
+++ Sources/ChangelogGenCLI/ChangelogGenCommand.swift
@@ -4,7 +4,7 @@ public struct ChangelogGenCommand: ParsableCommand {
     public static var configuration: CommandConfiguration = CommandConfiguration(
         commandName: "changeloggen",
         abstract: "A changelog generator.",
-        version: "0.0.4",
+        version: "0.0.5",
         shouldDisplay: true,
         subcommands: [GenerateCommand.self],
         defaultSubcommand: GenerateCommand.self)
